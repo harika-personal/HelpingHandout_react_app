fefd85fb86bfd5156e109c03a4028a85ce184e83
HarikaBale <harikabale.98@gmail.com>
2024-02-03 17:04:43 -0500

    project structure

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import React from 'react';
 import { Router, Routes, Route, Navigate, HashRouter } from 'react-router-dom';
 import Test from './test/test';
+import LandingPage from './pages';
 
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/test"/>}/>
+        <Route path="/" element={<Navigate to="/home"/>}/>
+        <Route path="/home" element={<LandingPage/>}/>
         <Route path="/test" element={<Test/>} />
       </Routes>
 
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import image from '../assets/WebsiteImage.png';
+
+const LandingPage = () => {
+
+
+    return (
+        <div>
+            <img
+                src={image}
+                alt="HelpingHandout.png"
+            />
+
+        </div>
+    );
+
+};
+export default LandingPage;
