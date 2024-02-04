6ac35f23ae29067eb41d1a78f19783a20c1c1260
HarikaBale <harikabale.98@gmail.com>
2024-02-03 21:03:56 -0500

    adjusted cards to match figma's layout

--- src/components/ArticlesCard.js
+++ src/components/ArticlesCard.js
@@ -4,19 +4,20 @@ const ArticlesCard = ({ image, link, headline, source }) => {
 
     return (
         <div className="Articles-card">
-            <div className="card " style={{ width: '19rem', height: '19rem' }}>
+            <div className="card " style={{ width: '435px', height: '470px' }}>
                 {/* Clickable card, you can add an onClick handler to the whole card */}
                 <a href={link}
                     style={{ textDecoration: 'none', color: 'inherit' }}>
+                    <div className="gradient-overlay">
                     <img src={image} alt="Card Image" className="card-image" />
+                    </div>
+                    
 
                     {/* Text on the top left corner */}
-                    <div className="position-absolute top-0 start-0 p-3"
-                        style={{ color: 'white', fontSize: '16px', fontWeight: 'bold' }}>
+                    <div className="position-absolute top-0 start-0 p-3 card-text ">
                         {headline}
                     </div>
-                    <div className="bottom-0 end-0 p-3 card-source"
-                    > {source}
+                    <div className="end-0 card-source"> {source}
                     </div>
                 </a>
             </div>
