05f9f25ee498652c4294d593e04d92b5aab15cb9
HarikaBale <harikabale.98@gmail.com>
2024-02-03 21:22:58 -0500

    fixed about us layout

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 import React from 'react';
 import { Router, Routes, Route, Navigate, HashRouter } from 'react-router-dom';
 import Test from './test/test';
-// import AboutUS from './pages/AboutUs';
-import Articles from './components/Articles';
+// import Articles from './components/Articles';
 import NavigationBar from './components/NavBar';
+import AboutUS from './components/AboutUs';
 
 
 function App() {
@@ -14,8 +14,8 @@ function App() {
       <NavigationBar/>
       <Routes>
         <Route path="/" element={<Navigate to="/home"/>}/>
-        {/* <Route path="/home" element={<AboutUS/>}/> */}
-        <Route path="/home" element={<Articles/>}/>
+        <Route path="/home" element={<AboutUS/>}/>
+        {/* <Route path="/home" element={<Articles/>}/> */}
         <Route path="/home/home/test" element={<Test/>} />
         <Route path="/listings" element={<Test/>} />
       </Routes>
--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,4 +1,5 @@
 import image from '../assets/WebsiteImage.png';
+import text from '../assets/aboutUSText.png';
 import '../styles/AboutUs.css';
 
 const AboutUS = () => {
@@ -6,23 +7,27 @@ const AboutUS = () => {
 
     return (
         <div className = "Inspiration">
-            <div className='row'>
-                <img className='col-3 mr -2'
+            {/* <div> */}
+                <div style={{width:'495'}}>
+                <img className='mr -2 aboutus-logo'
                     src={image}
                     alt="HelpingHandout.png"
-                >
-                    
-                </img>
-                <div className='col-9 mr-4 aboutUs'>
-                    <div className='mt-4'>
+                />
+                </div>
+                <img className='mr -2 aboutus-text'
+                    src={text}
+                    alt="Inspiration.png"
+                />
+                {/* <div className='col-9 mr-4 aboutus-text'>
+                    <div className='aboutus-text-1'>
                         Whether you’re a large corporation or a small business with “too many of one thing”, join us to find communities that value these items that need a home! If you’re looking for a community of change makers, volunteer with us!
                     </div>
 
-                    <div className='mt-4'>
+                    <div className='aboutus-text-2'>
                         Welcome! We’re Helping Handout, a community of donors and receivers on a mission to create a more sustainable world by encouraging a culture of “handing out”.
                     </div>
-                </div>
-            </div>
+                </div> */}
+            {/* </div> */}
 
 
 
