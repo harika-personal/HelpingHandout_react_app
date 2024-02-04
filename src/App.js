772f0affdd7eef99c26929999d2cf497a8d1a162
Cassie Guo <cassieguo441@gmail.com>
2024-02-03 23:52:39 -0800

    added the Landing Page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Test from './test/test';
 // import Articles from './components/Articles';
 import NavigationBar from './components/NavBar';
 import AboutUS from './components/AboutUs';
-
+import LandingPage from './pages/LandingPage';
+import FlashVolunteerList from './components/FlashVolunteer';
 
 function App() {
   return (
@@ -14,8 +15,8 @@ function App() {
       <NavigationBar/>
       <Routes>
         <Route path="/" element={<Navigate to="/home"/>}/>
-        <Route path="/home" element={<AboutUS/>}/>
-        {/* <Route path="/home" element={<Articles/>}/> */}
+        <Route path="/home" element={<LandingPage/>}/>
+        <Route path="/volunteerwithus" element={<FlashVolunteerList/>}/>
         <Route path="/home/home/test" element={<Test/>} />
         <Route path="/listings" element={<Test/>} />
       </Routes>
