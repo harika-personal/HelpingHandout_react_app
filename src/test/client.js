c201f7ee3b75969d06981db541ca1b8a96e002ce
HarikaBale <harikabale.98@gmail.com>
2024-02-04 21:01:21 -0500

    changes env variable for server

--- src/test/client.js
+++ src/test/client.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-export const BASE_API = process.env.REACT_APP_BASE_API_URL || "http://localhost:4000";
+// export const BASE_API = process.env.REACT_APP_BASE_API_URL || "http://localhost:4000";
+export const BASE_API = process.env.REACT_APP_BASE_API_URL;
 export const TEST_API = `${BASE_API}/test`;
 
 export const getUser = async () => {
